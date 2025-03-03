d204472dc3bd7ab877dc864ee65e1383dc5bce4a
T4LLER <taller@plastimaxsa.com>
2025-03-03 15:53:27 -0600

    Trabajo subcarpetas

---
+++ Creacion_subcarpeta.py
@@ -0,0 +1,8 @@
+import os
+
+os.chdir("//NAS1.plastimaxsa.local/Repuestos/Maquinaria Planta")#Comando donde se ingresa la direccion en donde se va a trabajar
+direccion_local = os.getcwd() #devuelve la dirección en donde estamos
+elementos_dereccion = os.listdir() #Pone en una clase lista los elemtenos que estan en esa dirección
+a = 0
+elementos = a in b for elementos_dereccion
+ 
