37eb9937ef9254e2b5c8329d504a64cfd5698390
T4LLER <taller@plastimaxsa.com>
2025-03-04 10:58:58 -0600

    Creacion inicial

--- Creacion_subcarpeta.py
+++ Creacion_subcarpeta.py
@@ -1,30 +1,18 @@
 import os
 
-#dirección = "/Users/josed/OneDrive/Escritorio/carpeta de prueba" # Dirección donde se haran las modificaciones
-dirección = "\Users\josed\OneDrive\Escritorio\carpeta de prueba"
-#numero = len(dirección)
+dirección = r"\\NAS1.plastimaxsa.local\Repuestos\Maquinaria Planta\Maquinas auxiliares\Hornos" # Dirección donde se haran las modificaciones
+dirección = dirección.replace("\\", "/")#Remplaza los caracteres para que se puedan leer
+os.chdir(dirección) #Comando donde se ingresa la direccion en donde se va a trabajar
+Lista_carpetas = os.listdir()#Se gurada en nombre de los archivos que estan en la dirección seleccionada
+Lista_direcciones = [f"{dirección}{"/"}{Lista_carpetas[a]}" for a in range(len(Lista_carpetas))]#Crea lista con las direcciones a utilizar para creación de carmpetas
+print(Lista_direcciones)
+Lista_sub_carptetas = ["Planos","Fotografias"]#Ingresar nombre de carpetas a crear en subcarpetas
 
-cadena_ascii = [ord(a) for a in dirección]
-
-print(cadena_ascii)
-
-#os.chdir(dirección) #Comando donde se ingresa la direccion en donde se va a trabajar
-#reconstruccion = [dirección[a] for a in range(numero-17)]#Reconstruye la dirección quitando la ultima palabra para cambiar de dirección 
-#base = "".join(reconstruccion)#concatena todos los valores captados por la LISTA COMPRIMIDA
-#elementos_dereccion = os.listdir() #Pone en una clase lista los elemtenos que estan en esa dirección a trabajar
-#parejas = ["{}{}".format(base,elementos_dereccion[a]) for a in range(len(elementos_dereccion))]
-
-#Lista_sub_carptetas = ["hola","como","estas"]
-
-#print(parejas)
-
-#for a in range(len(parejas)):
-#    os.chdir(parejas[a])
-#    for b in range(len(Lista_sub_carptetas)):
-#        os.mkdir(Lista_sub_carptetas[b])
-#        print(Lista_sub_carptetas[b])
+for a in range(len(Lista_direcciones)):
+    os.chdir(Lista_direcciones[a])
+    for b in range(len(Lista_sub_carptetas)):
+        os.mkdir(Lista_sub_carptetas[b])
+        print(Lista_sub_carptetas[b])
    
-#os.chdir(dirección) #Comando donde se ingresa la direccion en donde se va a trabajar
-#print(os.listdir())
 #direccion_local = os.getcwd() #devuelve la dirección en donde estamos
 
