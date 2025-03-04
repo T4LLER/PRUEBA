4e0dcb027ef50817b6d9561c4e7f9e1e0e8107eb
T4LLER <taller@plastimaxsa.com>
2025-03-03 18:32:02 -0600

    casa

--- Creacion_subcarpeta.py
+++ Creacion_subcarpeta.py
@@ -4,5 +4,6 @@ os.chdir("//NAS1.plastimaxsa.local/Repuestos/Maquinaria Planta")#Comando donde s
 direccion_local = os.getcwd() #devuelve la dirección en donde estamos
 elementos_dereccion = os.listdir() #Pone en una clase lista los elemtenos que estan en esa dirección
 a = 0
-elementos = a in b for elementos_dereccion
+elementos = a in b for elementos_dereccion # esto necesita cambiarse
+
  
